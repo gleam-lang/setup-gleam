bd98a8a6d63d0c72368fec2f36529703d069a003
Louis Pilfold <louis@lpil.uk>
2019-11-15 19:27:47 +0000

    Checkpoint

--- action.sh
+++ action.sh
@@ -5,5 +5,13 @@ export ASDF_DATA_DIR=/root/.asdf
 VERSION=$1
 
 $ASDF_DATA_DIR/bin/asdf install gleam $VERSION
+
+env
+
+echo
+echo
+echo $GITHUB_WORKSPACE
+ls $GITHUB_WORKSPACE
+
 cp -v $ASDF_DATA_DIR/installs/gleam/$VERSION/bin/gleam /usr/local/bin/gleam
 ls /usr/local/bin
