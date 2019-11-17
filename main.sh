5c9f67f20fb5e899f69d5150c6b6f6d4480219c3
Louis Pilfold <louis@lpil.uk>
2019-11-17 15:19:50 +0000

    Checkpoint

--- main.sh
+++ main.sh
@@ -6,12 +6,12 @@ export ASDF_DATA_DIR=$(mktemp -d)
 
 VERSION=$1
 
-# Install asdf
-git clone https://github.com/asdf-vm/asdf.git $ASDF_DATA_DIR --depth 1 --branch v0.7.5
+# Install asdf v0.7.5
+git clone https://github.com/asdf-vm/asdf.git $ASDF_DATA_DIR --depth 1 --branch c3ad7e0357816228fb12d761ba7d79729eb040ce
 source $ASDF_DATA_DIR/asdf.sh
 
-# Install asdf Gleam support
-asdf plugin-add gleam https://github.com/vic/asdf-gleam.git@e5ef3d477e384065190bc2c2d147971c98f03bad
+# Install asdf Gleam plugin
+asdf plugin-add gleam https://github.com/vic/asdf-gleam.git --branch e5ef3d477e384065190bc2c2d147971c98f03bad
 
 # Install Gleam
 asdf install gleam $VERSION
